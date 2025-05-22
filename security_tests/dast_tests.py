ded3395af7612d30e6c70dd242a91f3eaff5ed41
franciscop101 <franciscopereira101@gmail.com>
2025-05-22 22:14:13 +0100

    test

--- security_tests/dast_tests.py
+++ security_tests/dast_tests.py
@@ -2,7 +2,7 @@
 import requests
 import os
 
-API_URL = os.getenv("API_URL", "http://localhost:8501")
+API_URL = os.getenv("API_URL", "http://localhost:8000")
 
 def print_result(name, response):
     print(f"\n== {name} ==")
